dda14298eeeac1129d5cdb4b6b789286f568a3ba
Ankandas14 <ankandas659076@gmail.com>
2025-08-21 00:17:04 +0530

    Added

--- template.py
+++ template.py
@@ -5,7 +5,7 @@ import logging
 #logging string
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
-project_name = 'classification-pipeline'
+project_name = 'cnnClassifier'
 
 list_of_files = [
     ".github/workflows/.gitkeep",
